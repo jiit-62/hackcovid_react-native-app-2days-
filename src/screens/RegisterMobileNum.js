c33fe57565fea948f7a9a2ed23315cbb7c42a527
AnshikaAgrawal <anshikaagrawal1089@gmail.com>
2020-04-15 00:02:03 +0530

    local push notification is working(raw model)

--- src/screens/RegisterMobileNum.js
+++ src/screens/RegisterMobileNum.js
@@ -1,11 +1,35 @@
 import React from "react";
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements'
+import PushNotification from 'react-native-push-notification'
+import NotifService from "../components/NotifService"
 
 class RegisterMobileNum extends React.Component {
-  state = {
-    phoneno: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      phoneno: ""
+    };
+
+    this.notif = new NotifService(this.onRegister.bind(this), this.onNotif.bind(this));
   }
+
+
+  onRegister(token) {
+    Alert.alert("Registered !", JSON.stringify(token));
+    console.log(token);
+    // this.setState({ registerToken: token.token, gcmRegistered: true });
+  }
+
+  onNotif(notif) {
+    console.log(notif);
+    Alert.alert(notif.title, notif.message);
+  }
+
+  handlePerm(perms) {
+    Alert.alert("Permissions", JSON.stringify(perms));
+  }
+
   render() {
     return (
       <>
@@ -15,11 +39,20 @@ class RegisterMobileNum extends React.Component {
               this.setState({ phoneno })
             }
           }
-        }} /></View>
-        <View style={styles.spacing}><Button
-          title="Register"
-          onPress={() => this.props.navigation.navigate("Try", { phoneno: this.state.phoneno })}
-          disabled={(this.state.phoneno.length > 0) ? false : true} />
+        }} />
+        </View>
+        <View style={styles.spacing}>
+          <Button
+            title="Register"
+            onPress={() => this.props.navigation.navigate("Try", { phoneno: this.state.phoneno })}
+            disabled={(this.state.phoneno.length > 0) ? false : true} />
+        </View>
+
+        <View style={styles.spacing}>
+          <Button
+            title="Local Notification (now)"
+            onPress={() => { this.notif.localNotif() }}
+          />
         </View>
       </>
     )
